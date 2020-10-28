3a96fb9e4e0de27a7a3e690e8f7cd67ef37d3511
Alex Sutton <alexsutton14@hotmail.com>
2020-10-28 10:45:24 +0000

    Improving forms

--- src/components/employer.js
+++ src/components/employer.js
@@ -17,9 +17,35 @@ class Employer extends React.Component{
         }
     }
 
+    handleUpdate = (e) => {
+        const name = e.target.name;
+        let value = e.target.value;
+
+        console.log(value)
+
+        if (name === "startDate" || name === "endDate"){
+            value = new Date.parse(value);
+        }
+    
+        this.setState({
+            [name]: value
+          }
+        );
+    }
+
+    DateForInput(date){
+        if (date === undefined){
+            return "1970-01-01";
+        }
+        let output = "";
+        output = date.getFullYear() + "-" + (date.getMonth()+1) + "-" + date.getDay();
+        console.log(output);
+        return output;
+    }
+
     render(){
         if (this.props.locked){
-            let endDateString = "Present"
+            let endDateString;
             if (!this.state.current){
                 if (this.state.endDate instanceof Date){
                     endDateString = new Intl.DateTimeFormat('en-GB', {month: 'long', year: 'numeric'}).format(this.state.endDate);
@@ -32,19 +58,23 @@ class Employer extends React.Component{
                 <span>{new Intl.DateTimeFormat('en-GB', {month: 'long', year: 'numeric'}).format(this.state.startDate)} - {endDateString}</span>
             </div>
         } else {
+            
+            let startDateString = this.DateForInput(this.state.startDate);
+            let endDateString = this.DateForInput(this.state.endDate);
+
             return <form>
                 <h3>Employment {this.props.index + 1}</h3>
-                <div class="form-element">
-                    <label htmlFor="job-title">Job Title</label>
-                    <input id="job-title" type="text" value={this.state.jobTitle} onChange={this.props.changeEvent}></input>
+                <div className="form-element">
+                    <label htmlFor="jobTitle">Job Title</label>
+                    <input name="jobTitle" type="text" value={this.state.jobTitle} onChange={this.handleUpdate}></input>
                 </div>
-                <div class="form-element">
-                    <label htmlFor="employer-name">Employer</label>
-                    <input id="employer-name" type="text" value={this.state.employerName}></input>
+                <div className="form-element">
+                    <label htmlFor="employerName">Employer</label>
+                    <input name="employerName" type="text" value={this.state.employerName} onChange={this.handleUpdate}></input>
                 </div>
-                <div class="form-element">
+                <div className="form-element">
                     <label htmlFor="start-date">Start Date</label>
-                    <input id="start-date" type="date" value={this.state.startDate}></input>
+                    <input name="startDate" type="date" value={startDateString} onChange={this.handleUpdate}></input>
                 </div>
             </form>
         }
